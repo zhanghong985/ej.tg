b741dea93c681488521b213fc4011535b55d7615
hushuai123321 <1098997319@qq.com>
2020-01-08 18:02:55 +0800

    订单和产品管理更新

--- src/pages/product/List.vue
+++ src/pages/product/List.vue
@@ -64,7 +64,7 @@ layout="prev, pager, next"
     action="http://134.175.154.93:6677/file/upload"
     :on-success="uploadsuccessHandler"
     :file-list="fileList"
-    list-type="picture">
+    list-type="picture"> 
 <el-button size="small" type="primary">点击上传</el-button>
 <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
 </el-upload>
